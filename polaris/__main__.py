64b0f279482ac710f92d4bba69b7ba737c1ecc6c
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-02-12 02:23:30 -0700

    Add minimum framework for polaris list

---
+++ polaris/__main__.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+from polaris import list
+from polaris.version import __version__
+
+
+def main():
+    """
+    Entry point for the main script ``polaris``
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Perform polaris operations",
+        usage='''
+polaris <command> [<args>]
+
+The available polaris commands are:
+    list    List the available test cases
+    setup   Set up a test case
+    clean   Clean up a test case
+    suite   Manage a regression test suite
+    run     Run a suite, test case or step
+
+ To get help on an individual command, run:
+
+    polaris <command> --help
+    ''')
+
+    parser.add_argument('command', help='command to run')
+    parser.add_argument('-v', '--version',
+                        action='version',
+                        version=f'polaris {__version__}',
+                        help="Show version number and exit")
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(0)
+
+    args = parser.parse_args(sys.argv[1:2])
+
+    commands = {'list': list.main}
+
+    if args.command not in commands:
+        print('Unrecognized command {}'.format(args.command))
+        parser.print_help()
+        exit(1)
+
+    # call the function associated with the requested command
+    commands[args.command]()
+
+
+if __name__ == "__main__":
+    main()
---
+++ polaris/components.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from polaris.component import Component
+
+# import new components here
+# from polaris.ocean import Ocean
+
+
+def get_components():
+    """
+    Get a list of components, which in turn contain lists of test groups
+
+    Returns
+    -------
+    components : list of polaris.Component
+        A list of components containing all available tests
+    """
+    # add new components here
+    components: List[Component] = [
+        # Ocean(),
+    ]
+    return components
