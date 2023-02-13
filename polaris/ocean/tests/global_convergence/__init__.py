2828a2388b09321bd91c124c6267be9f972193af
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-02-12 22:38:44 -0700

    Add cosine bell test case

--- polaris/ocean/tests/global_convergence/__init__.py
+++ polaris/ocean/tests/global_convergence/__init__.py
@@ -1,6 +1,5 @@
+from polaris.ocean.tests.global_convergence.cosine_bell import CosineBell
 from polaris.testgroup import TestGroup
-
-# from polaris.ocean.tests.global_convergence.cosine_bell import CosineBell
 
 
 class GlobalConvergence(TestGroup):
@@ -15,6 +14,6 @@ class GlobalConvergence(TestGroup):
         """
         super().__init__(component=component, name='global_convergence')
 
-        # for icosahedral in [False, True]:
-        #     self.add_test_case(CosineBell(test_group=self,
-        #                                   icosahedral=icosahedral))
+        for icosahedral in [False, True]:
+            self.add_test_case(CosineBell(test_group=self,
+                                          icosahedral=icosahedral))
---
+++ polaris/ocean/tests/global_convergence/cosine_bell/init.py
@@ -0,0 +1,45 @@
+from polaris.model import run_model
+from polaris.step import Step
+
+
+class Init(Step):
+    """
+    A step for an initial condition for for the cosine bell test case
+    """
+    def __init__(self, test_case, mesh_name):
+        """
+        Create the step
+
+        Parameters
+        ----------
+        test_case : polaris.ocean.tests.global_convergence.cosine_bell.CosineBell  # noqa: E501
+            The test case this step belongs to
+
+        mesh_name : str
+            The name of the mesh
+        """
+
+        super().__init__(test_case=test_case,
+                         name=f'{mesh_name}_init',
+                         subdir=f'{mesh_name}/init',
+                         ntasks=36, min_tasks=1)
+
+        package = 'polaris.ocean.tests.global_convergence.cosine_bell'
+
+        self.add_namelist_file(package, 'namelist.init', mode='init')
+        self.add_streams_file(package, 'streams.init', mode='init')
+
+        self.add_input_file(filename='mesh.nc', target='../mesh/mesh.nc')
+
+        self.add_input_file(filename='graph.info', target='../mesh/graph.info')
+
+        self.add_model_as_input()
+
+        self.add_output_file(filename='namelist.ocean')
+        self.add_output_file(filename='initial_state.nc')
+
+    def run(self):
+        """
+        Run this step of the testcase
+        """
+        run_model(self)
