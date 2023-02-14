cd8f78820ff462e0edffd3f51a98ac3c34c9e70a
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-02-13 18:27:47 -0700

    Add versions side bar

--- docs/conf.py
+++ docs/conf.py
@@ -124,8 +124,14 @@ html_title = ""
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
 
-smv_tag_whitelist = r"^v\d+\.\d+.\d+$"  # Include tags like "tags/v2.5.0"
+html_sidebars = {
+    "**": [
+        "versions.html",
+    ],
+}
+
+smv_tag_whitelist = r"^\d+\.\d+.\d+$"  # Include tags like "tags/2.5.0"
 smv_branch_whitelist = "main"
 smv_remote_whitelist = r"^(origin|upstream)$"  # Use branches from origin
