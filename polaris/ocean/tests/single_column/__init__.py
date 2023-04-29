ff99bf9e82c9178f85043532d8fa2c46a2b6225b
Carolyn Begeman <cbegeman@lanl.gov>
2023-04-29 08:30:24 -0500

    Change resolution

--- polaris/ocean/tests/single_column/__init__.py
+++ polaris/ocean/tests/single_column/__init__.py
@@ -13,4 +13,4 @@ class SingleColumn(TestGroup):
         """
         super().__init__(component=component, name='single_column')
 
-        self.add_test_case(CVMix(test_group=self, resolution=10.))
+        self.add_test_case(CVMix(test_group=self, resolution=960.))
