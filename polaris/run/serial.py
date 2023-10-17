04227f10bf2e7f80a08d8158e309cadcffa51e48
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-10-17 12:55:47 +0200

    Set config.filepath after loading new config options
    Before this merge, we were not setting a config's `filepath`
    attribute after loading the config options from a file.  This
    causes the step to fail for cryptic reasons if it is rerun later
    as part of a test suite (because it failed the first time).

--- polaris/run/serial.py
+++ polaris/run/serial.py
@@ -66,9 +66,9 @@ def run_tasks(suite_name, quiet=False, is_task=False, steps_to_run=None,
     # get the config file for the first task in the suite
     task = next(iter(suite['tasks'].values()))
     component = task.component
-    config_filename = \
-        f'{task.base_work_dir}/{component.name}/{component.name}.cfg'
-    common_config = setup_config(config_filename)
+    common_config = setup_config(task.base_work_dir,
+                                 component.name,
+                                 f'{component.name}.cfg')
     available_resources = get_available_parallel_resources(common_config)
 
     # start logging to stdout/stderr
@@ -140,10 +140,9 @@ def run_single_step(step_is_subprocess=False, quiet=False):
     if step_is_subprocess:
         step.run_as_subprocess = False
 
-    config_filename = os.path.join(step.base_work_dir,
-                                   step.component.name,
-                                   step.config.filepath)
-    config = setup_config(config_filename)
+    config = setup_config(step.base_work_dir,
+                          step.component.name,
+                          step.config.filepath)
     task.config = config
     available_resources = get_available_parallel_resources(config)
     set_cores_per_node(task.config, available_resources['cores_per_node'])
@@ -302,10 +301,9 @@ def _log_and_run_task(task, stdout_logger, task_logger, quiet,
 
         os.chdir(task.work_dir)
 
-        config_filename = os.path.join(task.base_work_dir,
-                                       task.component.name,
-                                       task.config.filepath)
-        config = setup_config(config_filename)
+        config = setup_config(task.base_work_dir,
+                              task.component.name,
+                              task.config.filepath)
         task.config = config
         set_cores_per_node(task.config,
                            available_resources['cores_per_node'])
@@ -389,11 +387,9 @@ def _run_task(task, available_resources):
 
         step_start = time.time()
 
-        config_filename = os.path.join(step.base_work_dir,
-                                       step.component.name,
-                                       step.config.filepath)
-
-        step.config = setup_config(config_filename)
+        step.config = setup_config(step.base_work_dir,
+                                   step.component.name,
+                                   step.config.filepath)
         if task.log_filename is not None:
             step_log_filename = task.log_filename
         else:
