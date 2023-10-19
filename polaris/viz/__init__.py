66dc16a3f0ddf1452768a8c29450e01fe7797cea
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-10-19 11:43:07 +0200

    Add function for plotting global MPAS fields
    This approach uses uxarray and holoviews to plot efficiently
    on the native grid.

--- polaris/viz/__init__.py
+++ polaris/viz/__init__.py
@@ -1,3 +1,6 @@
 from polaris.viz.planar import plot_horiz_field
-from polaris.viz.spherical import plot_global_field
+from polaris.viz.spherical import (
+    plot_global_lat_lon_field,
+    plot_global_mpas_field,
+)
 from polaris.viz.style import use_mplstyle
