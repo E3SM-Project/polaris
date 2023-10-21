6dcbc3bd3ec1bef1be16a95eb0ef0bd0612c0c0f
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-10-21 16:44:12 +0200

    Add ice_shelf module to ocean framework
    This currently computes land-ice pressure and freezing temperature.

---
+++ polaris/ocean/ice_shelf/__init__.py
@@ -0,0 +1,6 @@
+from polaris.ocean.ice_shelf.freeze import compute_freezing_temperature
+from polaris.ocean.ice_shelf.pressure import (
+    compute_land_ice_draft_from_pressure,
+    compute_land_ice_pressure_from_draft,
+    compute_land_ice_pressure_from_thickness,
+)
