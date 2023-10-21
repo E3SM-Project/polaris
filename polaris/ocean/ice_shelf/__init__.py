681e01c341a3b47e332ee65c84ddf166a9f3846c
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-10-21 16:44:12 +0200

    Add land-ice pressure and freezing temperature to ice_shelf module

--- polaris/ocean/ice_shelf/__init__.py
+++ polaris/ocean/ice_shelf/__init__.py
@@ -1,6 +1,12 @@
 from typing import Dict
 
-from polaris import Step, Task
+from polaris import Task
+from polaris.ocean.ice_shelf.freeze import compute_freezing_temperature
+from polaris.ocean.ice_shelf.pressure import (
+    compute_land_ice_draft_from_pressure,
+    compute_land_ice_pressure_from_draft,
+    compute_land_ice_pressure_from_thickness,
+)
 from polaris.ocean.ice_shelf.ssh_adjustment import SshAdjustment
 from polaris.ocean.ice_shelf.ssh_forward import SshForward
 
