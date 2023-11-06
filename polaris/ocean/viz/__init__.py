4ca3362c4fbab07633247e41fa62a3b23cbeb8c8
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-11-06 14:48:50 +0100

    Switch transect viz to use polaris transects on quads

--- polaris/ocean/viz/__init__.py
+++ polaris/ocean/viz/__init__.py
@@ -1,1 +1,2 @@
-from polaris.ocean.viz.transect import compute_transect, plot_transect
+from polaris.ocean.viz.transect.plot import plot_transect
+from polaris.ocean.viz.transect.vert import compute_transect
