f224416f55e6dc0c44722856d3d731e067796ae3
Carolyn Begeman <cbegeman@lanl.gov>
2024-04-16 18:08:21 -0500

    Re-enable barotropic drying_slope cases

--- polaris/ocean/tasks/drying_slope/baroclinic/__init__.py
+++ polaris/ocean/tasks/drying_slope/baroclinic/__init__.py
@@ -4,13 +4,15 @@ from polaris.ocean.tasks.drying_slope.init import Init
 from polaris.ocean.tasks.drying_slope.viz import Viz
 
 
-class LinearDrying(Task):
+class Baroclinic(Task):
     """
-    The linear drying version of the drying slope test case creates the mesh
+    The baroclinic version of the drying slope test case creates the mesh
     and initial condition, then performs a short forward run on 4 cores.
     """
 
-    def __init__(self, component, resolution, indir, coord_type='sigma',
+    def __init__(self, component, resolution, init, subdir,
+                 coord_type='sigma',
+                 forcing_type='linear_drying', method='ramp',
                  drag_type='constant_and_rayleigh',
                  time_integrator='split_explicit'):
         """
@@ -24,30 +26,32 @@ class LinearDrying(Task):
         resolution : float
             The resolution of the test case in km
 
-        indir : str
-            The directory the task is in, to which ``name`` will be appended
+        subdir : str
+            TODO
 
         init : polaris.ocean.tasks.drying_slope.init.Init
             A shared step for creating the initial state
         """
+        name = f'baroclinic_{method}'
+        if drag_type == 'loglaw':
+            name = f'{name}_{drag_type}'
+            subdir = f'{subdir}_{drag_type}'
+        super().__init__(component=component, name=name, subdir=subdir)
         self.coord_type = coord_type
-        super().__init__(component=component, name='linear_drying',
-                         indir=indir)
 
+        self.add_step(init, symlink='init')
         self.add_step(
-            Init(component=component, indir=self.subdir, resolution=resolution,
-                 baroclinic=True, drag_type=drag_type))
-
-        self.add_step(
-            Forward(component=component, indir=self.subdir, ntasks=None,
+            Forward(component=component, indir=subdir, subdir=None,
+                    ntasks=None,
                     min_tasks=None, openmp_threads=1, resolution=resolution,
-                    forcing_type='linear_drying', coord_type=coord_type,
+                    forcing_type=forcing_type, coord_type=coord_type,
                     time_integrator=time_integrator, drag_type=drag_type,
-                    damping_coeff=1.0e-4, baroclinic=True))
+                    damping_coeff=1.0e-4, baroclinic=True, method=method))
 
         self.add_step(
-            Viz(component=component, indir=self.subdir, damping_coeffs=None,
-                baroclinic=True, forcing_type='linear'))
+            Viz(component=component, indir=subdir, subdir=None,
+                damping_coeffs=None,
+                baroclinic=True, forcing_type=forcing_type))
 
     def configure(self):
         """
