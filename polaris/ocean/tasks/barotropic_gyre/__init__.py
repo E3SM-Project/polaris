cba0fa40f7ecfc35eee10009c792dfe72c75e908
Carolyn Begeman <cbegeman@lanl.gov>
2024-11-01 19:05:35 -0500

    Reorg task and steps

--- polaris/ocean/tasks/barotropic_gyre/__init__.py
+++ polaris/ocean/tasks/barotropic_gyre/__init__.py
@@ -11,7 +11,15 @@ def add_barotropic_gyre_tasks(component):
     component : polaris.ocean.Ocean
         the ocean component that the task will be added to
     """
-    component.add_task(BarotropicGyre(component=component))
+    test_name = 'default'
+    subdir = f'planar/barotropic_gyre/{test_name}'
+    config_filename = 'barotropic_gyre.cfg'
+    config = PolarisConfigParser(filepath=f'{subdir}/{config_filename}')
+    config.add_from_package('polaris.ocean.tasks.barotropic_gyre',
+                            config_filename)
+    component.add_task(BarotropicGyre(component=component,
+                                      test_name=test_name,
+                                      config=config))
 
 
 class BarotropicGyre(Task):
@@ -19,7 +27,7 @@ class BarotropicGyre(Task):
     The convergence test case for inertial gravity waves
     """
 
-    def __init__(self, component):
+    def __init__(self, component, test_name, config, smoke_test=False):
         """
         Create the test case
 
@@ -28,17 +36,27 @@ class BarotropicGyre(Task):
         component : polaris.ocean.Ocean
             The ocean component that this task belongs to
         """
-        name = 'barotropic_gyre'
-        subdir = f'planar/{name}'
+        group_name = 'barotropic_gyre'
+        name = f'{group_name}_{test_name}'
+        subdir = f'planar/{group_name}/{test_name}'
         super().__init__(component=component, name=name, subdir=subdir)
-        init_step = Init(component=component, subdir=self.subdir)
-        self.add_step(init_step)
 
         config_filename = 'barotropic_gyre.cfg'
-        self.config.add_from_package('polaris.ocean.tasks.barotropic_gyre',
-                                     config_filename)
-        self.add_step(
-            Forward(component=component, indir=self.subdir, ntasks=None,
-                    min_tasks=None, openmp_threads=1,
-                    run_time_steps=3,
-                    graph_target=f'{init_step.path}/culled_graph.info'))
+
+        init = Init(component=component, subdir=subdir)
+        init.set_shared_config(config, link=config_filename)
+        self.add_step(init)
+
+        forward = Forward(component=component, indir=self.subdir, ntasks=None,
+                          min_tasks=None, openmp_threads=1,
+                          name='short_forward', run_time_steps=3,
+                          graph_target=f'{init.path}/culled_graph.info')
+        forward.set_shared_config(config, link=config_filename)
+        self.add_step(forward)
+
+        forward = Forward(component=component, indir=self.subdir, ntasks=None,
+                          min_tasks=None, openmp_threads=1,
+                          name='long_forward',
+                          graph_target=f'{init.path}/culled_graph.info')
+        forward.set_shared_config(config, link=config_filename)
+        self.add_step(forward, run_by_default=False)
