4dc06b182bb4b54b85aeb673a6cfb733358bf8f3
Carolyn Begeman <cbegeman@lanl.gov>
2024-11-03 18:34:04 -0600

    Clean-up initial state step

--- polaris/ocean/tasks/barotropic_gyre/init.py
+++ polaris/ocean/tasks/barotropic_gyre/init.py
@@ -31,7 +31,8 @@ class Init(Step):
         """
         super().__init__(component=component, name='init', indir=subdir)
 
-        for file in ['base_mesh.nc', 'culled_mesh.nc', 'culled_graph.info']:
+        for file in ['base_mesh.nc', 'culled_mesh.nc', 'culled_graph.info',
+                     'forcing.nc']:
             self.add_output_file(file)
         self.add_output_file('initial_state.nc',
                              validate_vars=['layerThickness'])
@@ -62,7 +63,7 @@ class Init(Step):
         # vertical coordinate parameters
         bottom_depth = config.getfloat('vertical_grid', 'bottom_depth')
         # coriolis parameters
-        f_0 = config.getfloat("barotropic_gyre", "f_0")
+        f0 = config.getfloat("barotropic_gyre", "f_0")
         beta = config.getfloat("barotropic_gyre", "beta")
         # surface (wind) forcing parameters
         tau_0 = config.getfloat("barotropic_gyre", "tau_0")
@@ -89,23 +90,32 @@ class Init(Step):
 
         # set the coriolis values
         for loc in ["Cell", "Edge", "Vertex"]:
-            ds[f"f{loc}"] = f_0 + beta * ds[f"y{loc}"]
+            ds[f"f{loc}"] = f0 + beta * ds[f"y{loc}"]
+        ds.attrs['nx'] = nx
+        ds.attrs['ny'] = ny
+        ds.attrs['dc'] = dc
 
         # set the initial condition for normalVelocity
-        ds["normalVelocity"] = xr.zeros_like(ds.xEdge).expand_dims(
-            ["Time", "nVertLevels"],
-            axis=[0, -1])
+        normal_velocity, _ = xr.broadcast(
+            xr.zeros_like(ds_mesh.xEdge), ds.refBottomDepth)
+        normal_velocity = normal_velocity.transpose('nEdges', 'nVertLevels')
+        normal_velocity = normal_velocity.expand_dims(dim='Time', axis=0)
+        ds['normalVelocity'] = normal_velocity
 
         # write the initial condition file
-        write_netcdf(ds_mesh, 'initial_state.nc')
+        write_netcdf(ds, 'init.nc')
 
         # set the wind stress forcing
-        ds_forcing = ds_mesh.copy()
+        ds_forcing = xr.Dataset()
         # Convert from km to m
         ly = ly * 1e3
+        wind_stress_zonal = -tau_0 * \
+            np.cos(np.pi * (ds.yCell - ds.yCell.min()) / ly)
+        wind_stress_meridional = xr.zeros_like(ds.xCell)
         ds_forcing["windStressZonal"] = \
-            -tau_0 * np.cos(np.pi * (ds.yCell - ds.yCell.min()) / ly)
-        ds_forcing["windStressMeridional"] = xr.zeros_like(ds.xCell)
+            wind_stress_zonal.expand_dims(dim='Time', axis=0)
+        ds_forcing["windStressMeridional"] = \
+            wind_stress_meridional.expand_dims(dim='Time', axis=0)
         write_netcdf(ds_forcing, 'forcing.nc')
 
         cell_mask = ds.maxLevelCell >= 1
@@ -114,46 +124,3 @@ class Init(Step):
                          'forcing_wind_stress_zonal.png', cmap='cmo.balance',
                          show_patch_edges=True, cell_mask=cell_mask,
                          vmin=-0.1, vmax=0.1)
-
-
-def exact_ssh_solution(
-        ds, tau_0=0.1, rho=1.e3, g=9.81, nu_2=4.e2, beta=1e-11, f_0=1e-4):
-    """
-    Exact solution to the sea surface height for the linearized Munk layer
-    experiments.
-
-    Parameters
-    ----------
-    ds : xarray.Dataset
-        Must contain the fields: `xCell`, `yCell`, ....
-    tau_0 : Float
-        .... [N m-2]
-    rho : Float
-        Constant ocean density [kg m-3]
-    g : Float
-        Gravitational acceleration constant [m s-2]
-    nu_2 : Float
-        Viscosity [m2 s-1]
-    beta : Float
-        ... [s-1 m-1]
-    f_0 : Float
-        ... [s-1]
-    """
-
-    xCell = ds.xCell
-    yCell = ds.yCell
-    L_x = float(xCell.max() - xCell.min())
-    L_y = float(yCell.max() - yCell.min())
-    layerThickness = ds.restingThickness.squeeze()
-
-    pi = np.pi
-    sqrt3 = np.sqrt(3)
-    delta_m = (nu_2 / beta)**(1. / 3.)
-    gamma = (sqrt3 * ds.xCell) / (2. * delta_m)
-
-    ssh = (tau_0 / (rho * g * layerThickness)) * (ds.fCell / beta) *\
-          (1. - ds.xCell / L_x) * pi * np.sin(pi * ds.yCell / L_y) *\
-          (1. - np.exp(-1. * ds.xCell / (2. * delta_m)) *
-           (np.cos(gamma) + (1. / sqrt3) * np.sin(gamma)))
-
-    return ssh
