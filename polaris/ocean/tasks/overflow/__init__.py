4d49dd144f3867c0f9692d46084f71a40dc55b9e
Carolyn Begeman <cbegeman@lanl.gov>
2025-03-27 17:32:17 -0500

    Add overflow test with init step

---
+++ polaris/ocean/tasks/overflow/__init__.py
@@ -0,0 +1,53 @@
+from polaris import Task
+from polaris.config import PolarisConfigParser as PolarisConfigParser
+from polaris.ocean.tasks.overflow.init import Init as Init
+
+
+def add_overflow_tasks(component):
+    """
+    Add a task TODO
+
+    component : polaris.ocean.Ocean
+        the ocean component that the task will be added to
+    """
+    basedir = 'planar/overflow'
+    config_filename = 'overflow.cfg'
+    filepath = f'{basedir}/{config_filename}'
+    config = PolarisConfigParser(filepath=filepath)
+    config.add_from_package('polaris.ocean.tasks.overflow', config_filename)
+    component.add_task(Overflow(component=component, config=config))
+
+
+class Overflow(Task):
+    """
+    The overflow test case TODO
+    """
+
+    def __init__(self, component, config):
+        """
+        Create the test case
+
+        Parameters
+        ----------
+        component : polaris.ocean.Ocean
+            The ocean component that this task belongs to
+
+        config : polaris.config.PolarisConfigParser
+            A shared config parser
+        """
+        test_name = 'default'
+        basedir = 'planar/overflow'
+        subdir = f'{basedir}/{test_name}'
+        name = f'overflow_{test_name}'
+        config_filename = 'manufactured_solution.cfg'
+
+        super().__init__(component=component, name=name, subdir=subdir)
+        self.set_shared_config(config, link=config_filename)
+        subdir = f'{basedir}/init'
+        symlink = 'init'
+        if subdir in component.steps:
+            init_step = component.steps[subdir]
+        else:
+            init_step = Init(component=component, name='init')
+            init_step.set_shared_config(self.config, link=config_filename)
+        self.add_step(init_step, symlink=symlink)
---
+++ polaris/ocean/tasks/overflow/init.py
@@ -0,0 +1,134 @@
+import numpy as np
+import xarray as xr
+from mpas_tools.mesh.conversion import convert, cull
+from mpas_tools.planar_hex import make_planar_hex_mesh
+
+from polaris.mesh.planar import compute_planar_hex_nx_ny
+from polaris.ocean.model import OceanIOStep
+from polaris.ocean.vertical import init_vertical_coord
+
+
+class Init(OceanIOStep):
+    """
+    A step for creating a mesh and initial condition for the nonhydro
+    and the hydro vs nonhydro test cases.
+    """
+
+    def __init__(self, component, name='init', subdir=None):
+        """
+        Create the step
+
+        Parameters
+        ----------
+        test_case : compass.TestCase
+        """
+        super().__init__(component=component, name=name, subdir=subdir)
+
+    def setup(self):
+        super().setup()
+        output_filenames = ['base_mesh.nc', 'culled_mesh.nc', 'init.nc']
+        model = self.config.get('ocean', 'model')
+        if model == 'mpas-ocean':
+            output_filenames.append('culled_graph.info')
+        for filename in output_filenames:
+            self.add_output_file(filename=filename)
+
+    def run(self):
+        """
+        Run this step of the test case
+        """
+        config = self.config
+        logger = self.logger
+
+        section = config['overflow']
+        lx = section.getfloat('lx')
+        ly = section.getfloat('ly')
+        resolution = section.getfloat('resolution')
+
+        nx, ny = compute_planar_hex_nx_ny(lx, ly, resolution)
+        dc = 1e3 * resolution
+        ds_mesh = make_planar_hex_mesh(
+            nx=nx, ny=ny, dc=dc, nonperiodic_x=True, nonperiodic_y=False
+        )
+        self.write_model_dataset(ds_mesh, 'base_mesh.nc')
+
+        ds_mesh = cull(ds_mesh, logger=logger)
+        ds_mesh = convert(
+            ds_mesh, graphInfoFileName='culled_graph.info', logger=logger
+        )
+        self.write_model_dataset(ds_mesh, 'culled_mesh.nc')
+
+        max_bottom_depth = section.getfloat('shelf_depth')
+        shelf_depth = section.getfloat('shelf_depth')
+        x_slope = section.getfloat('x_slope')
+        L_slope = section.getfloat('L_slope')
+
+        ds = ds_mesh.copy()
+        ds['bottomDepth'] = -(
+            -max_bottom_depth
+            + 0.5
+            * (max_bottom_depth - shelf_depth)
+            * (1.0 + np.tanh((nx * dc - ds.xCell - x_slope) / L_slope))
+        )
+        # ssh
+        ds['ssh'] = xr.zeros_like(ds.xCell)
+
+        init_vertical_coord(config, ds)
+
+        # initial salinity and temperature
+        salinity = section.getfloat('salinity')
+        ds['salinity'] = (salinity * xr.ones_like(ds.zMid)).where(ds.cellMask)
+
+        # T = Tref - (rho - rhoRef)/alpha
+        x_dense = section.getfloat('x_dense')
+        lower_temperature = section.getfloat('lower_temperature')
+        higher_temperature = section.getfloat('higher_temperature')
+        x, _ = np.meshgrid(np.zeros(ds.sizes['nVertLevels']), ds.xCell)
+        temperature = np.where(
+            x < x_dense, lower_temperature, higher_temperature
+        )
+        temperature = np.where(ds.cellMask, temperature, 0.0)
+        ds['temperature'] = (
+            (
+                'Time',
+                'nCells',
+                'nVertLevels',
+            ),
+            np.expand_dims(temperature, axis=0),
+        )
+
+        # initial velocity on edges
+        ds['normalVelocity'] = (
+            (
+                'Time',
+                'nEdges',
+                'nVertLevels',
+            ),
+            np.zeros([1, ds.sizes['nEdges'], ds.sizes['nVertLevels']]),
+        )
+
+        # Coriolis parameter
+        ds['fCell'] = (
+            (
+                'nCells',
+                'nVertLevels',
+            ),
+            np.zeros([ds.sizes['nCells'], ds.sizes['nVertLevels']]),
+        )
+        ds['fEdge'] = (
+            (
+                'nEdges',
+                'nVertLevels',
+            ),
+            np.zeros([ds.sizes['nEdges'], ds.sizes['nVertLevels']]),
+        )
+        ds['fVertex'] = (
+            (
+                'nVertices',
+                'nVertLevels',
+            ),
+            np.zeros([ds.sizes['nVertices'], ds.sizes['nVertLevels']]),
+        )
+
+        # finalize and write file
+        self.write_model_dataset(ds, 'init.nc')
