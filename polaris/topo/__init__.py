5497424a3f2b66d0e05ef6947a65c535d65c0e15
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-04-14 00:41:04 -0500

    Add a task for combining topo
    This is just a little "wrapper" task to allow us to run combine
    topo without caching, and then cache the results.

--- polaris/topo/__init__.py
+++ polaris/topo/__init__.py
@@ -1,1 +1,3 @@
-from polaris.topo.combine import Combine as Combine
+from polaris.topo.combine import CombineStep as CombineStep
+from polaris.topo.combine import CombineTask as CombineTask
+from polaris.topo.combine import get_combine_topo_step as get_combine_topo_step
