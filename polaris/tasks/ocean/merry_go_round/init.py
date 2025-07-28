bb60c5b9b0cb617a41ca1f709140c0acab3f7246
Carolyn Begeman <cbegeman@lanl.gov>
2025-07-28 12:58:18 -0500

    Fixup IC

--- polaris/tasks/ocean/merry_go_round/init.py
+++ polaris/tasks/ocean/merry_go_round/init.py
@@ -64,8 +64,8 @@ class Init(OceanIOStep):
         vert_coord = config.get('vertical_grid', 'coord_type')
         bottom_depth = config.getfloat('vertical_grid', 'bottom_depth')
 
-        if not (vert_coord == 'z-level' or vert_coord == 'sigma'):
-            raise ValueError('Vertical coordinate {vert_coord} not supported')
+        # if not (vert_coord == 'z-level' or vert_coord == 'sigma'):
+        #    raise ValueError('Vertical coordinate {vert_coord} not supported')
 
         # get reference resolution and number of layers
         ref_nz = config.getint('vertical_grid', 'vert_levels')
@@ -93,19 +93,12 @@ class Init(OceanIOStep):
         write_netcdf(ds_mesh, 'culled_mesh.nc')
 
         ds = ds_mesh.copy()
-        x_cell_adjusted = ds.xCell - ds.xEdge.min()
-        x_edge_adjusted = ds.xEdge - ds.xEdge.min()
 
         ds['ssh'] = xr.zeros_like(ds.xCell)
         ds['bottomDepth'] = bottom_depth * xr.ones_like(ds.xCell)
 
         config.set('vertical_grid', 'vert_levels', str(nz))
         init_vertical_coord(config, ds)
-
-        x_min = x_cell_adjusted.min()
-        x_max = x_cell_adjusted.max()
-        x_mid = 0.5 * (x_min + x_max)
-        z_mid = ds.zMid.squeeze('Time')
 
         # Only the top layer moves in this test case
         ds['vertCoordMovementWeights'] = xr.ones_like(ds.refZMid)
@@ -114,12 +107,19 @@ class Init(OceanIOStep):
             ds['vertCoordMovementWeights'][:] = 0.0
             ds['vertCoordMovementWeights'][0] = 1.0
 
-        # ...
+        # Fix the x-offset for initial condition functions
+        x_min_edge = ds.xEdge.min()
+        x_cell_adjusted = ds.xCell - x_min_edge
+        x_edge_adjusted = ds.xEdge - x_min_edge
+        x_min = x_cell_adjusted.min()
+        x_max = x_cell_adjusted.max()
+        lx_model = x_max - x_min
+
         x_cell_2D, _ = xr.broadcast(x_cell_adjusted, ds.refBottomDepth)
         x_edge_2D, _ = xr.broadcast(x_edge_adjusted, ds.refBottomDepth)
-        angle_edge_2D, _ = xr.broadcast(ds.angleEdge, ds.refBottomDepth)
 
         # Initialize temperature
+        x_mid = 0.5 * lx_model
         temperature = temperature_right * xr.ones_like(x_cell_2D)
         temperature = xr.where(
             x_cell_2D < x_mid, temperature_left, temperature_right
@@ -131,39 +131,37 @@ class Init(OceanIOStep):
         ds['salinity'] = salinity_background * xr.ones_like(temperature)
 
         # Initialize normalVelocity
+        z_mid = ds.zMid.squeeze('Time')
         z_mid_edge = z_mid.isel(nCells=ds.cellsOnEdge - 1).mean('TWO')
 
-        x_one_quarters = x_min + 0.25 * (x_max - x_min)
-        x_three_quarters = x_min + 0.75 * (x_max - x_min)
-        x_cell_on_edge = ds.xCell.isel(nCells=ds.cellsOnEdge - 1)
+        # Define the streamfunctions for the velocity field
+        # (we include the vertical streamfunction and velocity for reference)
+        psi1 = 1 - ((x_edge_2D - 0.5 * lx_model) ** 4 / (0.5 * lx_model) ** 4)
+        # psi2 = 1 - ((z_mid_edge + 0.5 * bottom_depth) ** 2 /
+        #             (0.5 * bottom_depth) ** 2)
 
-        condition_1 = (x_cell_on_edge > x_three_quarters).all('TWO')
-        condition_2 = (
-            (x_cell_on_edge < x_mid) & (x_cell_on_edge >= x_one_quarters)
-        ).all('TWO')
+        # dpsi1 = - ((4 * x_edge_2D - 2. * lx_model) ** 3 /
+        #            (0.5 * lx_model) ** 4)
+        dpsi2 = -(2.0 * z_mid_edge + bottom_depth) / (0.5 * bottom_depth) ** 2
 
-        mask_2D, _ = xr.broadcast(condition_1 | condition_2, ds.refBottomDepth)
+        u = psi1 * dpsi2
+        # w = dpsi1 * psi2
 
-        d_psi = -(2.0 * z_mid_edge + bottom_depth) / (0.5 * bottom_depth) ** 2
-
-        den = (0.5 * (x_max - x_min)) ** 4
-        num = xr.where(
-            mask_2D,
-            (x_edge_2D - x_min - 0.5 * (x_max + x_min)) ** 4,
-            (x_edge_2D - 0.5 * x_max) ** 4,
-        )
-
-        normal_velocity = (1 - (num / den)) * (d_psi * np.cos(angle_edge_2D))
+        normal_velocity = u * np.cos(ds.angleEdge)
+        # We set the normal velocity to zero at the horizontal boundaries
         normal_velocity = xr.where(
             (x_edge_2D <= x_min) | (x_edge_2D >= x_max), 0, normal_velocity
         )
         ds['normalVelocity'] = normal_velocity.expand_dims(dim='Time', axis=0)
 
         # Initialize debug tracers
-        half_depth = 0.5 * bottom_depth
-        psi1 = 1.0 - ((x_cell_2D - 0.5 * x_max) ** 4 / (0.5 * x_max) ** 4)
-        psi2 = 1.0 - ((z_mid + half_depth) ** 2 / (half_depth) ** 2)
-        psi = psi1 * psi2
+        psi1_cell = 1.0 - (
+            (x_cell_2D - 0.5 * lx_model) ** 4 / (0.5 * lx_model) ** 4
+        )
+        psi2_cell = 1.0 - (
+            (z_mid + 0.5 * bottom_depth) ** 2 / (0.5 * bottom_depth) ** 2
+        )
+        psi = psi1_cell * psi2_cell
 
         ds['tracer1'] = xr.zeros_like(temperature)
         ds['tracer1'].isel(Time=0)[:] = 0.5 * (1 + np.tanh(2 * psi - 1))
