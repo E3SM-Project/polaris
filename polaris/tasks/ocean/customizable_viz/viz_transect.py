87ffe611f02928b7548432bb0afa3cf8bb93351b
Carolyn Begeman <cbegeman@lanl.gov>
2025-09-26 10:40:50 -0500

    Move viz helper functions outside task

--- polaris/tasks/ocean/customizable_viz/viz_transect.py
+++ polaris/tasks/ocean/customizable_viz/viz_transect.py
@@ -1,47 +1,27 @@
 import cmocean  # noqa: F401
 import numpy as np
 import xarray as xr
-import datetime
 from mpas_tools.ocean.viz.transect import compute_transect, plot_transect
 
 from polaris.ocean.model import OceanIOStep as OceanIOStep
-from polaris.tasks.ocean.customizable_viz.viz import get_viz_defaults
+from polaris.viz import get_viz_defaults
 
 
 class VizTransect(OceanIOStep):
     def __init__(self, component, name, indir):
         super().__init__(component=component, name=name, indir=indir)
 
-    def setup(self):
-        section = self.config['customizable_viz']
-        mesh_file = section.get('mesh_file')
-        input_file = section.get('input_file')
-        print(input_file)
-        self.add_input_file(
-            filename='input.nc',
-            target=input_file,
-        )
-        # self.add_input_file(
-        #    filename='mesh.nc',
-        #    target=mesh_file,
-        # )
-        #for var_name in self.variables:
-        #    self.add_output_file(f'{var_name}_transect_{self.time_stamp}.png')
-
-    #def dynamic_model_config(self):
     def runtime_setup(self):
-        print('runtime_setup')
         section = self.config['customizable_viz']
         self.mesh_file = section.get('mesh_file')
         self.input_file = section.get('input_file')
 
     def run(self):
         section_name = 'customizable_viz_transect'
-        variables = self.config.getlist(
-            section_name, 'variables', dtype=str
-        )
-        #time_stamp = self.config.get(section_name, 'time_stamp')
+        variables = self.config.getlist(section_name, 'variables', dtype=str)
         section = self.config[section_name]
+        # time_stamp = section.get('time_stamp')
+        layer_interface_color = section.get('layer_interface_color')
         x_start = section.getfloat('x_start')
         x_end = section.getfloat('x_end')
         y_start = section.getfloat('y_start')
@@ -54,13 +34,14 @@ class VizTransect(OceanIOStep):
         ds = self.open_model_dataset(self.input_file, decode_timedelta=False)
         # tidx = np.argwhere(ds['xtime_startMonthly'].values == time_stamp)
         t_index = 0
-        # time_stamp = self.time_stamp
         if 'xtime_startMonthly' in ds.keys():
             start_time = ds['xtime_startMonthly'].values[t_index].decode()
-            #time_stamp = datetime.datetime.strptime(start_time, '%Y-%m-%d_%H:%M:%S')
             time_stamp = start_time.split('_')[0]
 
-        ds = ds.isel(Time=t_index)  # , nVertLevelsP1=slice(0, -1))
+        ds = ds.isel(Time=t_index)
+        # Transect is constructed for nVertLevels quantities
+        if 'nVertLevelsP1' in ds.sizes:
+            ds = ds.isel(nVertLevelsP1=slice(0, -1))
         if 'timeSeriesStatsMonthly' in self.input_file:
             prefix = 'timeMonthly_avg_'
         else:
@@ -87,7 +68,7 @@ class VizTransect(OceanIOStep):
                 vmax = section.getfloat('vmax')
             else:
                 vmax = np.percentile(mpas_field.values, 95)
-            if vmax > 0. and vmin < 0.:
+            if vmax > 0.0 and vmin < 0.0:
                 vmax = max(abs(vmax), abs(vmin))
                 vmin = -vmax
             if var_name in viz_dict.keys():
@@ -101,8 +82,7 @@ class VizTransect(OceanIOStep):
                 mpas_field=mpas_field,
                 title=f'{var_name}',
                 out_filename=f'{var_name}_transect_{time_stamp}.png',
-                #interface_color='gray',
-                interface_color='none',
+                interface_color=layer_interface_color,
                 vmin=vmin,
                 vmax=vmax,
                 cmap=cmap,
--- polaris/viz/__init__.py
+++ polaris/viz/__init__.py
@@ -1,11 +1,14 @@
+from polaris.viz.helper import (
+    get_projection as get_projection,
+)
+from polaris.viz.helper import (
+    get_viz_defaults as get_viz_defaults,
+)
 from polaris.viz.planar import plot_horiz_field as plot_horiz_field
 from polaris.viz.spherical import (
     plot_global_lat_lon_field as plot_global_lat_lon_field,
 )
 from polaris.viz.spherical import (
     plot_global_mpas_field as plot_global_mpas_field,
-)
-from polaris.viz.spherical import (
-    plot_polar_mpas_field as plot_polar_mpas_field,
 )
 from polaris.viz.style import use_mplstyle as use_mplstyle
