5e63ecfbf4883548a7b79b0c791ab810f3951a82
Carolyn Begeman <cbegeman@lanl.gov>
2025-09-26 11:39:42 -0500

    Add option to use target z-level for plotting based on mean resting thickness

--- polaris/tasks/ocean/customizable_viz/viz_horiz_field.py
+++ polaris/tasks/ocean/customizable_viz/viz_horiz_field.py
@@ -2,36 +2,15 @@ import cmocean  # noqa: F401
 import numpy as np
 
 from polaris.ocean.model import OceanIOStep
-from polaris.tasks.ocean.customizable_viz.viz import (
+from polaris.viz import (
     get_viz_defaults,
+    plot_global_mpas_field,
 )
-from polaris.viz import plot_polar_mpas_field
 
 
 class VizHorizField(OceanIOStep):
     def __init__(self, component, name, indir):
         super().__init__(component=component, name=name, indir=indir)
-
-    # def setup(self):
-    #    section = self.config['customizable_viz']
-    #    # mesh_file = section.get('mesh_file')
-    #    input_file = section.get('input_file')
-    #    self.add_input_file(
-    #        filename='input.nc',
-    #        target=input_file,
-    #    )
-    #    # self.add_input_file(
-    #    #    filename='mesh.nc',
-    #    #    target=mesh_file,
-    #    # )
-    #    section_name = 'customizable_viz_horiz_field'
-    #    self.variables = self.config.getlist(
-    #        section_name, 'variables', dtype=str
-    #    )
-    #    self.time_stamp = self.config.get(section_name, 'time_stamp')
-
-    #    for var_name in self.variables:
-    #        self.add_output_file(f'{var_name}_horiz_{self.time_stamp}.png')
 
     def runtime_setup(self):
         section = self.config['customizable_viz']
@@ -43,26 +22,12 @@ class VizHorizField(OceanIOStep):
         section = self.config[section_name]
 
         # Determine the projection from the config file
-        # projection_name = section.get('projection')
+        projection_name = section.get('projection')
         central_longitude = section.getfloat('central_longitude')
-        # projection = get_projection(projection_name,
-        #                            central_longitude=central_longitude)
-
-        # These settings are determined by the MPAS-Ocean mesh
-        # transform = cartopy.crs.Geodetic()
-        # use_latlon = True
 
         # Descriptor is none for the first variable and assigned thereafter
         descriptor = None
-        # ds_mesh = self.open_model_dataset(self.mesh_file)
-        # Shouldn't be necessary because mesh should have attr is_spherical
-        # ds_mesh.attrs['is_periodic'] = 'NO'
-        # descriptor = mosaic.Descriptor(
-        #    ds_mesh,
-        #    projection=projection,
-        #    transform=transform,
-        #    use_latlon=use_latlon,
-        # )
+
         ds = self.open_model_dataset(self.input_file, decode_timedelta=False)
         if 'timeSeriesStatsMonthly' in self.input_file:
             prefix = 'timeMonthly_avg_'
@@ -81,9 +46,21 @@ class VizHorizField(OceanIOStep):
         # different from the file frequency
         # tidx = np.argwhere(ds['xtime'].values == time_stamp)
         ds = ds.isel(Time=t_index)
-        z_index = 0
-        # z_index = np.argmin(np.abs(ds['zMid'].values - z_target))
-
+        # z_index = 0
+        z_target = section.getfloat('z_target')
+        ds_mesh = self.open_model_dataset(
+            self.mesh_file, decode_timedelta=False
+        )
+        z_bottom = ds_mesh['restingThickness'].cumsum(dim='nVertLevels')
+        dz = z_bottom.mean(dim='nCells') - z_target
+        z_index = np.argmin(np.abs(dz.values))
+        if dz[z_index] > 0 and z_index > 0:
+            z_index -= 1
+        z_mean = z_bottom.mean(dim='nCells')[z_index].values
+        print(
+            f'Using z_index {z_index} for z_target {z_target} '
+            f'with mean depth {z_mean} '
+        )
         viz_dict = get_viz_defaults()
         variables = self.config.getlist(section_name, 'variables', dtype=str)
         min_latitude = section.getfloat('min_latitude')
@@ -92,16 +69,22 @@ class VizHorizField(OceanIOStep):
         max_longitude = section.getfloat('max_longitude')
 
         for var_name in variables:
-            full_var_name = f'{prefix}{var_name}'
+            if 'accumulated' in var_name:
+                full_var_name = var_name
+            else:
+                full_var_name = f'{prefix}{var_name}'
             if full_var_name not in ds.keys():
                 print(
                     f'Skipping {full_var_name}, not found in {self.input_file}'
                 )
                 continue
-
+            print(f'Plotting {full_var_name}')
+            filename_suffix = ''
             mpas_field = ds[full_var_name]
             if 'nVertLevels' in mpas_field.sizes:
                 mpas_field = mpas_field.isel(nVertLevels=z_index)
+                if z_index != 0:
+                    filename_suffix = f'_z{z_index}'
 
             if self.config.has_option(section_name, 'vmin'):
                 vmin = section.getfloat('vmin')
@@ -111,7 +94,7 @@ class VizHorizField(OceanIOStep):
                 vmax = section.getfloat('vmax')
             else:
                 vmax = np.percentile(mpas_field.values, 95)
-            if vmin < 0.0:
+            if vmin < 0.0 or 'Tendency' in var_name or 'Flux' in var_name:
                 vmax = max(abs(vmax), abs(vmin))
                 vmin = -vmax
 
@@ -128,16 +111,16 @@ class VizHorizField(OceanIOStep):
                 value='{"vmin": ' + str(vmin) + ', "vmax": ' + str(vmax) + '}',
             )
 
-            # descriptor = plot_global_mpas_field(
-            descriptor = plot_polar_mpas_field(
+            descriptor = plot_global_mpas_field(
                 mesh_filename=self.mesh_file,
                 da=mpas_field,
-                out_filename=f'{var_name}_horiz_{time_stamp}.png',
+                out_filename=f'{var_name}_horiz_{time_stamp}{filename_suffix}.png',
                 config=self.config,
                 colormap_section='customizable_viz_horiz_field',
                 descriptor=descriptor,
                 colorbar_label=f'{var_name} {units}',
                 plot_land=True,
+                projection_name=projection_name,
                 central_longitude=central_longitude,
                 min_latitude=min_latitude,
                 max_latitude=max_latitude,
