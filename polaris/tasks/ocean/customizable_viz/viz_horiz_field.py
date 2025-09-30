406d71044d8870892d4eef440cef6b500585d9fd
Carolyn Begeman <cbegeman@lanl.gov>
2025-09-30 13:54:59 -0500

    Extend customizable viz so it works for mpas-a output as well

--- polaris/tasks/ocean/customizable_viz/viz_horiz_field.py
+++ polaris/tasks/ocean/customizable_viz/viz_horiz_field.py
@@ -17,7 +17,7 @@ class VizHorizField(OceanIOStep):
         self.mesh_file = section.get('mesh_file')
         self.input_file = section.get('input_file')
 
-    def run(self):
+    def run(self):  # noqa:C901
         section_name = 'customizable_viz_horiz_field'
         section = self.config[section_name]
 
@@ -28,29 +28,32 @@ class VizHorizField(OceanIOStep):
         # Descriptor is none for the first variable and assigned thereafter
         descriptor = None
 
-        ds = self.open_model_dataset(self.input_file, decode_timedelta=False)
-        if 'timeSeriesStatsMonthly' in self.input_file:
-            prefix = 'timeMonthly_avg_'
-            time_variable = 'xtime_startMonthly'
-        else:
-            print('did not find timeSeriesStatsMonthly in input_file name')
-            prefix = ''
-            time_variable = 'xtime'
-
-        t_index = 0
-        start_time = ds[time_variable].values[t_index].decode()
-        time_stamp = start_time.split('_')[0]
-
-        # TODO consider supporting setting a time_stamp value in the config
-        # file to enable extraction from streams whose output frequency is
-        # different from the file frequency
-        # tidx = np.argwhere(ds['xtime'].values == time_stamp)
-        ds = ds.isel(Time=t_index)
-        # z_index = 0
-        z_target = section.getfloat('z_target')
         ds_mesh = self.open_model_dataset(
             self.mesh_file, decode_timedelta=False
         )
+        min_latitude = section.getfloat('min_latitude')
+        max_latitude = section.getfloat('max_latitude')
+        min_longitude = section.getfloat('min_longitude')
+        max_longitude = section.getfloat('max_longitude')
+        lat_cell = ds_mesh['latCell'] * 180.0 / np.pi
+        lon_cell = ds_mesh['lonCell'] * 180.0 / np.pi
+        if min_longitude < lon_cell.min():
+            min_longitude += 360.0
+            max_longitude += 360.0
+        cell_indices = np.where(
+            (lat_cell >= min_latitude)
+            & (lat_cell <= max_latitude)
+            & (lon_cell >= min_longitude)
+            & (lon_cell <= max_longitude)
+        )
+        if len(cell_indices[0]) == 0:
+            raise ValueError(
+                'No cells found within the specified lat/lon bounds. '
+                'Please adjust the min/max latitude/longitude values.'
+            )
+        ds_mesh = ds_mesh.isel(nCells=cell_indices[0])
+        # z_index = 0
+        z_target = section.getfloat('z_target')
         z_bottom = ds_mesh['restingThickness'].cumsum(dim='nVertLevels')
         dz = z_bottom.mean(dim='nCells') - z_target
         z_index = np.argmin(np.abs(dz.values))
@@ -61,12 +64,45 @@ class VizHorizField(OceanIOStep):
             f'Using z_index {z_index} for z_target {z_target} '
             f'with mean depth {z_mean} '
         )
+
+        ds = self.open_model_dataset(self.input_file, decode_timedelta=False)
+        if 'timeSeriesStatsMonthly' in self.input_file:
+            prefix = 'timeMonthly_avg_'
+            time_variable = 'xtime_startMonthly'
+            has_time_variable = True
+        elif 'xtime' in ds.keys():
+            prefix = ''
+            time_variable = 'xtime'
+            has_time_variable = True
+        elif 'Time' in ds.keys():
+            prefix = 'timeMonthly_avg_'
+            time_variable = 'Time'
+            has_time_variable = True
+        else:
+            has_time_variable = False
+        if has_time_variable:
+            t_index = 0
+            start_time = ds[time_variable].values[t_index]
+            # if 'Time' not in ds.keys():
+            start_time = start_time.decode()
+            time_stamp = start_time.split('_')[0]
+            # else:
+            #    time_stamp = start_time.strftime('%Y-%m-%d')
+
+            # TODO consider supporting setting a time_stamp value in the config
+            # file to enable extraction from streams whose output frequency is
+            # different from the file frequency
+            # tidx = np.argwhere(ds['xtime'].values == time_stamp)
+            ds = ds.isel(Time=t_index)
+        ds = ds.isel(nCells=cell_indices[0])
+        if ds.sizes['nCells'] != ds_mesh.sizes['nCells']:
+            raise ValueError(
+                f'Number of cells in the mesh {ds_mesh.sizes["nCells"]} '
+                f'and input {ds.sizes["nCells"]} do not match. '
+            )
+        print(f'Using dataset with nCells={ds_mesh.sizes["nCells"]}')
         viz_dict = get_viz_defaults()
         variables = self.config.getlist(section_name, 'variables', dtype=str)
-        min_latitude = section.getfloat('min_latitude')
-        max_latitude = section.getfloat('max_latitude')
-        min_longitude = section.getfloat('min_longitude')
-        max_longitude = section.getfloat('max_longitude')
 
         for var_name in variables:
             if 'accumulated' in var_name:
@@ -74,10 +110,14 @@ class VizHorizField(OceanIOStep):
             else:
                 full_var_name = f'{prefix}{var_name}'
             if full_var_name not in ds.keys():
-                print(
-                    f'Skipping {full_var_name}, not found in {self.input_file}'
-                )
-                continue
+                if f'{prefix}activeTracers_{var_name}' in ds.keys():
+                    full_var_name = f'{prefix}activeTracers_{var_name}'
+                else:
+                    print(
+                        f'Skipping {full_var_name}, '
+                        f'not found in {self.input_file}'
+                    )
+                    continue
             print(f'Plotting {full_var_name}')
             filename_suffix = ''
             mpas_field = ds[full_var_name]
@@ -86,25 +126,37 @@ class VizHorizField(OceanIOStep):
                 if z_index != 0:
                     filename_suffix = f'_z{z_index}'
 
-            if self.config.has_option(section_name, 'vmin'):
-                vmin = section.getfloat('vmin')
+            if self.config.has_option(section_name, 'colormap_name'):
+                cmap = self.config.get(section_name, 'colormap_name')
             else:
-                vmin = np.percentile(mpas_field.values, 5)
-            if self.config.has_option(section_name, 'vmax'):
+                if var_name in viz_dict.keys():
+                    cmap = viz_dict[var_name]['colormap']
+                else:
+                    cmap = viz_dict['default']['colormap']
+                self.config.set(section_name, 'colormap_name', value=cmap)
+
+            if self.config.has_option(
+                section_name, 'vmin'
+            ) and self.config.has_option(section_name, 'vmax'):
+                vmin = section.getfloat('vmin')
                 vmax = section.getfloat('vmax')
             else:
-                vmax = np.percentile(mpas_field.values, 95)
-            if vmin < 0.0 or 'Tendency' in var_name or 'Flux' in var_name:
-                vmax = max(abs(vmax), abs(vmin))
-                vmin = -vmax
-
+                if (
+                    cmap == 'cmo.balance'
+                    or 'vertVelocityTop' in var_name
+                    or 'Tendency' in var_name
+                    or 'Flux' in var_name
+                ):
+                    vmax = max(abs(vmax), abs(vmin))
+                    vmin = -vmax
+                else:
+                    vmin = np.percentile(mpas_field.values, 5)
+                    vmax = np.percentile(mpas_field.values, 95)
             if var_name in viz_dict.keys():
-                cmap = viz_dict[var_name]['colormap']
                 units = viz_dict[var_name]['units']
             else:
-                cmap = viz_dict['default']['colormap']
                 units = viz_dict['default']['units']
-            self.config.set(section_name, 'colormap_name', value=cmap)
+
             self.config.set(
                 section_name,
                 'norm_args',
