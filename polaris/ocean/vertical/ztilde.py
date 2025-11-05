49e8b2b0a8f5598c980c5a462cd08bad3e3e1b8f
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-11-05 05:28:12 -0600

    Add a function for computing geometric z from z_tilde

--- polaris/ocean/vertical/ztilde.py
+++ polaris/ocean/vertical/ztilde.py
@@ -9,16 +9,13 @@ with z_tilde positive upward. Here, ``p`` is sea pressure in Pascals (Pa),
 ``mpas_tools.cime.constants``.
 """
 
-from typing import Union
-
 import xarray as xr
 from mpas_tools.cime.constants import constants
-
-ArrayLike = Union[xr.DataArray]
 
 __all__ = [
     'z_tilde_from_pressure',
     'pressure_from_z_tilde',
+    'z_from_z_tilde',
 ]
 
 
@@ -82,3 +79,66 @@ def pressure_from_z_tilde(z_tilde: xr.DataArray, rho0: float) -> xr.DataArray:
             'note': 'p = -z_tilde * (rho0 * g)',
         }
     )
+
+
+def z_from_z_tilde(
+    layer_thickness: xr.DataArray,
+    bottom_depth: xr.DataArray,
+    spec_vol: xr.DataArray,
+    rho0: float,
+) -> tuple[xr.DataArray, xr.DataArray]:
+    """
+    Compute geometric height z at layer interfaces and midpoints given the
+    layer thicknesses, bottom depth, specific volume and reference density.
+    This calculation assumes a constant specific volume within each layer.
+
+    Parameters
+    ----------
+    layer_thickness : xarray.DataArray
+        The pseudo thickness of each layer.
+
+    spec_vol : xarray.DataArray
+        The specific volume at each layer.
+
+    bottom_depth : xarray.DataArray
+        The positive-down depth of the seafloor.
+
+    rho0 : float
+        Reference density in kg m^-3.
+
+    Returns
+    -------
+    z_interface : xarray.DataArray
+        The elevation of layer interfaces.
+
+    z_mid : xarray.DataArray
+        The elevation of layer centers.
+    """
+
+    n_vert_levels = layer_thickness.sizes['nVertLevels']
+
+    z_bot = -bottom_depth
+    z_interface_list = [z_bot]
+    z_mid_list = []
+
+    for k in range(n_vert_levels - 1, -1, -1):
+        dz = (
+            rho0
+            * spec_vol.isel(nVertLevels=k)
+            * layer_thickness.isel(nVertLevels=k)
+        )
+        z_top = z_bot + dz
+        z_interface_list.append(z_top)
+        z_mid_list.append(z_bot + 0.5 * dz)
+        z_bot = z_top
+
+    dims = list(layer_thickness.dims)
+    interface_dims = list(dims) + ['nVertLevelsP1']
+    interface_dims.remove('nVertLevels')
+
+    z_interface = xr.concat(
+        reversed(z_interface_list), dim='nVertLevelsP1'
+    ).transpose(*interface_dims)
+    z_mid = xr.concat(reversed(z_mid_list), dim='nVertLevels').transpose(*dims)
+
+    return z_interface, z_mid
