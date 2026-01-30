64a138ad0495fb269853438898abc0c47dc5b04b
Carolyn Begeman <cbegeman@lanl.gov>
2026-01-30 12:27:34 -0600

    Support fetching time from xtime variable

--- polaris/ocean/time.py
+++ polaris/ocean/time.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import numpy as np
 import pandas as pd
 
@@ -9,6 +11,19 @@ def get_days_since_start(ds):
     """
     if 'daysSinceStartOfSim' in ds.keys():
         t_arr = ds.daysSinceStartOfSim.values.astype(float)
+    elif 'xtime' in ds.keys():
+        timestamps = []
+        for time_str in ds.xtime.values.astype(str):
+            try:
+                timestamp = datetime.strptime(time_str, '%Y-%m-%d_%H:%M:%S.%f')
+            except ValueError:
+                timestamp = datetime.strptime(time_str, '%Y-%m-%d_%H:%M:%S')
+            timestamps.append(timestamp)
+        # Calculate seconds since the first timestamp
+        seconds_since_start = [
+            (ts - timestamps[0]).total_seconds() for ts in timestamps
+        ]
+        t_arr = np.array(seconds_since_start, dtype=float) / 86400.0
     elif 'Time' in ds.keys():
         t_vals = ds['Time'].values
         t_pd = pd.to_datetime(t_vals)
