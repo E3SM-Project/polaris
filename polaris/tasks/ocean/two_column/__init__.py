3aab91ebc5cf7b59d16a0341cb824d88035db607
Xylar Asay-Davis <xylarstorm@gmail.com>
2026-02-02 10:29:25 +0100

    Add two-column test with z-tilde gradient

--- polaris/tasks/ocean/two_column/__init__.py
+++ polaris/tasks/ocean/two_column/__init__.py
@@ -1,4 +1,5 @@
 from polaris.tasks.ocean.two_column.salinity_gradient import SalinityGradient
+from polaris.tasks.ocean.two_column.ztilde_gradient import ZTildeGradient
 
 
 def add_two_column_tasks(component):
@@ -11,3 +12,4 @@ def add_two_column_tasks(component):
         the ocean component that the tasks will be added to
     """
     component.add_task(SalinityGradient(component=component))
+    component.add_task(ZTildeGradient(component=component))
