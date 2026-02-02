eb39f0be2a2fee136890735e99606e56f0b84923
Carolyn Begeman <cbegeman@lanl.gov>
2026-02-02 14:36:15 -0800

    Use function for computing pseudothickness from dataset in write model dataset

--- polaris/tasks/ocean/manufactured_solution/init.py
+++ polaris/tasks/ocean/manufactured_solution/init.py
@@ -69,7 +69,7 @@ class Init(OceanIOStep):
         ds_mesh = make_planar_hex_mesh(
             nx=nx, ny=ny, dc=dc, nonperiodic_x=False, nonperiodic_y=False
         )
-        self.write_model_dataset(ds_mesh, 'base_mesh.nc')
+        self.write_model_dataset(ds_mesh, 'base_mesh.nc', config)
 
         ds_mesh = cull(ds_mesh, logger=logger)
         ds_mesh = convert(
