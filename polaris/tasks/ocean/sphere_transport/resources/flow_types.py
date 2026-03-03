1fc674155368c957a53e72ae0f6e09c106114a6a
Carolyn Begeman <cbegeman@lanl.gov>
2026-03-03 09:59:25 -0800

    Add a function for computing normalVelocity from zonal, meridional components on edges

--- polaris/tasks/ocean/sphere_transport/resources/flow_types.py
+++ polaris/tasks/ocean/sphere_transport/resources/flow_types.py
@@ -1,4 +1,5 @@
 import numpy as np
+import xarray as xr
 from mpas_tools.transects import lon_lat_to_cartesian
 from numpy import cos, pi, sin
 
@@ -119,8 +120,25 @@ def flow_rotation(lon, lat, omega, tau, sphere_radius):
     omega = (2.0 * pi / tau) * (omega / np.linalg.norm(omega))
     x, y, z = lon_lat_to_cartesian(lon, lat, sphere_radius, degrees=False)
     xyz = np.stack((x, y, z), axis=1)
-    vel = np.cross(omega, np.divide(np.transpose(xyz), sphere_radius), axis=0)
+    vel = np.cross(omega, np.transpose(xyz), axis=0)
     east, north = calc_vector_east_north(x, y, z)
     u = np.sum(vel * east, axis=0)
     v = np.sum(vel * north, axis=0)
     return u, v
+
+
+def normal_velocity_from_zonal_meridional(
+    ds_mesh, u, v, recompute_angle_edge=False
+):
+    """
+    Compute normal velocity from zonal and meridional velocity components
+    defined on edges
+    """
+    if recompute_angle_edge:
+        angle_edge = recompute_angle_edge(ds_mesh)
+    else:
+        angle_edge = ds_mesh.angleEdge
+
+    normal_velocity = xr.zeros_like(angle_edge)
+    normal_velocity.values = u * np.cos(angle_edge) + v * np.sin(angle_edge)
+    return normal_velocity
