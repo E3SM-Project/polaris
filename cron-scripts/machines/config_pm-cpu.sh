1b702f59ee67f84fe2b117f879a8c89f6aa63310
Carolyn Begeman <cbegeman@lanl.gov>
2026-03-16 08:59:03 -0700

    Use USER env variable for paths

--- cron-scripts/machines/config_pm-cpu.sh
+++ cron-scripts/machines/config_pm-cpu.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 module load cray-python cmake
 
-export CRONJOB_BASEDIR=/pscratch/sd/y/youngsun/omega/cronjobs_pm-cpu
+export CRONJOB_BASEDIR=/pscratch/sd/${USER:0:1}/${USER}/omega/cronjobs_pm-cpu
 export E3SM_COMPILERS="gnu"
 
 mkdir -p "$CRONJOB_BASEDIR"
--- cron-scripts/machines/config_pm-gpu.sh
+++ cron-scripts/machines/config_pm-gpu.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 module load cray-python cmake
 
-export CRONJOB_BASEDIR=/pscratch/sd/y/youngsun/omega/cronjobs_pm-gpu
+export CRONJOB_BASEDIR=/pscratch/sd/${USER:0:1}/${USER}/omega/cronjobs_pm-gpu
 export E3SM_COMPILERS="gnugpu"
 
 mkdir -p "$CRONJOB_BASEDIR"
