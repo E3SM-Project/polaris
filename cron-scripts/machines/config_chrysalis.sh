c12641db5230332c3af41875ea064ff290fa15c0
Carolyn Begeman <cbegeman@lanl.gov>
2026-03-16 08:59:03 -0700

    Use USER env variable for paths

--- cron-scripts/machines/config_chrysalis.sh
+++ cron-scripts/machines/config_chrysalis.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 source /etc/bashrc
 
-export CRONJOB_BASEDIR=/lcrc/globalscratch/ac.kimy/cronjobs
+export CRONJOB_BASEDIR=/lcrc/globalscratch/${USER}/cronjobs
 export E3SM_COMPILERS="gnu intel"
 
 mkdir -p "$CRONJOB_BASEDIR"
--- cron-scripts/machines/config_frontier.sh
+++ cron-scripts/machines/config_frontier.sh
@@ -10,7 +10,7 @@ export http_proxy=http://proxy.ccs.ornl.gov:3128/
 export https_proxy=http://proxy.ccs.ornl.gov:3128/
 export no_proxy='localhost,127.0.0.0/8,*.ccs.ornl.gov'
 
-export CRONJOB_BASEDIR=/lustre/orion/cli115/scratch/grnydawn/cronjobs
+export CRONJOB_BASEDIR=/lustre/orion/cli115/scratch/${USER}/cronjobs
 export E3SM_COMPILERS="craygnu-mphipcc craycray-mphipcc crayamd-mphipcc craygnu craycray crayamd"
 
 mkdir -p "$CRONJOB_BASEDIR"
