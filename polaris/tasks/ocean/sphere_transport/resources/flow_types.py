41ab7f6d360548861edc46f1e26becaf4597699d
Carolyn Begeman <cbegeman@lanl.gov>
2026-03-17 10:57:36 -0700

    Use ds_mesh.sphere_radius for non-div, div

--- polaris/tasks/ocean/sphere_transport/resources/flow_types.py
+++ polaris/tasks/ocean/sphere_transport/resources/flow_types.py
@@ -8,7 +8,7 @@ from polaris.mesh.spherical import (
 )
 
 
-def flow_nondivergent(t, lon, lat, u_0, tau):
+def flow_nondivergent(t, lon, lat, u_0, tau, sphere_radius):
     """
     Compute a nondivergent velocity field
 
@@ -40,14 +40,14 @@ def flow_nondivergent(t, lon, lat, u_0, tau):
     lon_p = lon - 2.0 * pi * t / tau
     coslat = cos(lat)
     cost = cos(pi * t / tau)
-    u = (1 / tau) * (
-        u_0 * (sin(lon_p) ** 2) * sin(2 * lat) * cost + 2.0 * pi * coslat
+    u = (u_0 * sphere_radius / tau) * (
+        (sin(lon_p) ** 2) * sin(2 * lat) * cost + 2.0 * pi * coslat
     )
-    v = (u_0 / tau) * sin(2 * lon_p) * coslat * cost
+    v = (u_0 * sphere_radius / tau) * sin(2 * lon_p) * coslat * cost
     return u, v
 
 
-def flow_divergent(t, lon, lat, u_0, tau):
+def flow_divergent(t, lon, lat, u_0, tau, sphere_radius):
     """
     Compute a divergent velocity field
 
@@ -79,11 +79,13 @@ def flow_divergent(t, lon, lat, u_0, tau):
     lon_p = lon - 2.0 * pi * t / tau
     coslat = cos(lat)
     cost = cos(pi * t / tau)
-    u = (1 / tau) * (
-        -u_0 * (sin(lon_p / 2) ** 2) * sin(2 * lat) * (coslat**2) * cost
+    u = (u_0 * sphere_radius / tau) * (
+        -(sin(lon_p / 2) ** 2) * sin(2 * lat) * (coslat**2) * cost
         + 2.0 * pi * coslat
     )
-    v = (u_0 / (2 * tau)) * sin(lon_p) * (coslat**3) * cost
+    v = (u_0 * sphere_radius / (2 * tau)) * (
+        sin(lon_p) * (coslat**3) * cost
+    )
     return u, v
 
 
