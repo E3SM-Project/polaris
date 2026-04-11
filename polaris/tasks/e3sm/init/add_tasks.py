d6b5aa67f56c8ffe3799bd60e04d04c5949cca68
Xylar Asay-Davis <xylarstorm@gmail.com>
2026-04-11 10:48:03 +0200

    Add more lat-lon resolutions to topo/combine

--- polaris/tasks/e3sm/init/add_tasks.py
+++ polaris/tasks/e3sm/init/add_tasks.py
@@ -13,11 +13,16 @@ def add_e3sm_init_tasks(component):
     component : polaris.Component
         the e3sm/init component that the tasks will be added to
     """
-    for resolution in [3000, 120]:
+    for cubed_sphere_res in [3000, 120]:
         component.add_task(
-            CubedSphereCombineTask(component=component, resolution=resolution)
+            CubedSphereCombineTask(
+                component=component, resolution=cubed_sphere_res
+            )
         )
-    component.add_task(LatLonCombineTask(component=component, resolution=0.25))
+    for lat_lon_res in [0.0625, 0.25, 1.0]:
+        component.add_task(
+            LatLonCombineTask(component=component, resolution=lat_lon_res)
+        )
 
     add_remap_topo_tasks(component=component)
 
