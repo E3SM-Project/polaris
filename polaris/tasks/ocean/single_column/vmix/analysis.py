5811a529cc5309687a8eef0e7e68dd1bd2ef18a0
Carolyn Begeman <cbegeman@lanl.gov>
2026-04-26 13:29:19 -0700

    Fixup vmix analysis step for Omega support

--- polaris/tasks/ocean/single_column/vmix/analysis.py
+++ polaris/tasks/ocean/single_column/vmix/analysis.py
@@ -1,11 +1,13 @@
 import numpy as np
 
+from polaris.constants import get_constant
 from polaris.ocean.model import (
     OceanIOStep,
     get_days_since_start,
 )
-
-# TODO import rho_0 from constants
+from polaris.ocean.vertical import (
+    compute_zint_zmid_from_layer_thickness,
+)
 
 
 class Analysis(OceanIOStep):
@@ -25,11 +27,21 @@ class Analysis(OceanIOStep):
             if comparisons
             else {'forward': '../forward/output.nc'}
         )
-        for comparison_name, comparison_path in self.comparisons.items():
-            self.add_input_file(
-                filename=f'{comparison_name}_diags.nc',
-                target=f'{comparison_path}/output/KPP_test.0001-01-01_00.00.00.nc',
-            )
+
+    def setup(self):
+        model = self.config.get('ocean', 'model')
+        if model == 'mpas-ocean':
+            for comparison_name, comparison_path in self.comparisons.items():
+                self.add_input_file(
+                    filename=f'{comparison_name}.nc',
+                    target=f'{comparison_path}/output/KPP_test.0001-01-01_00.00.00.nc',
+                )
+        else:
+            for comparison_name, comparison_path in self.comparisons.items():
+                self.add_input_file(
+                    filename=f'{comparison_name}.nc',
+                    target=f'{comparison_path}/output.nc',
+                )
 
     def run(self):
         """
@@ -41,15 +53,18 @@ class Analysis(OceanIOStep):
             + section.getfloat('wind_stress_meridional') ** 2.0
         )
         # u_star = 0.01
-        rho_0 = 1026.0
-        u_star = wind_stress / rho_0
+        RhoSw = get_constant('seawater_density_reference')
+        u_star = wind_stress / RhoSw
         # TODO why is this 0
         # u_star = ds_diags_1day['surfaceFrictionVelocity'].mean(dim='nCells')
         # TODO compute this based on config parameters
         N_sq_init = 1.0e-4
+        ds_init = self.open_model_dataset(
+            '../init/initial_state.nc', decode_times=False
+        )
         for comparison_name in self.comparisons.keys():
             ds_diags = self.open_model_dataset(
-                f'{comparison_name}_diags.nc', decode_times=False
+                f'{comparison_name}.nc', decode_times=True
             )
             t_target = 1.0  # empirical relationship hold for up to 30h
             t_arr = get_days_since_start(ds_diags)
@@ -64,8 +79,27 @@ class Analysis(OceanIOStep):
                 1 / 3
             )
             ds_diags_1day = ds_diags.isel(Time=t_index)
-            z_top_final = ds_diags_1day['zTop'].mean(dim='nCells')
-            N_sq = ds_diags_1day['BruntVaisalaFreqTop'].mean(dim='nCells')
+            if 'zTop' in ds_diags_1day.keys():
+                z_top_final = ds_diags_1day['zTop'].mean(dim='nCells')
+            else:
+                z_int_final, _ = compute_zint_zmid_from_layer_thickness(
+                    layer_thickness=ds_diags_1day['layerThickness'],
+                    bottom_depth=ds_init['bottomDepth'],
+                    min_level_cell=ds_init['minLevelCell'] - 1,
+                    max_level_cell=ds_init['maxLevelCell'] - 1,
+                )
+                z_top_final = z_int_final[:-1].mean(dim='nCells')
+                z_top_final = z_top_final.rename(
+                    {'nVertLevelsP1': 'nVertLevels'}
+                )
+            if 'BruntVaisalaFreqTop' in ds_diags_1day.keys():
+                N_sq = ds_diags_1day['BruntVaisalaFreqTop'].mean(dim='nCells')
+            else:
+                self.logger.warn(
+                    f'BruntVailsalaFreqTop not present in ds {comparison_name}'
+                    ' skipping BLD comparison'
+                )
+                continue
             index_bld = int(np.nanargmax(N_sq.values))
             bld = z_top_final.isel(nVertLevels=index_bld)
             self.logger.info(
