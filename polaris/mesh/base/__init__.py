9f7c308f02264221c09b9193c071ed3b6956ebda
Xylar Asay-Davis <xylarstorm@gmail.com>
2026-05-10 05:59:36 -0500

    Reorg simple base meshes to allow listing just names
    This makes it much easier and more efficient to include these
    base meshes in downstream step factories

--- polaris/mesh/base/__init__.py
+++ polaris/mesh/base/__init__.py
@@ -1,6 +1,55 @@
-from typing import Dict, List, Tuple
+from types import MappingProxyType
+from typing import NamedTuple, Optional
 
-from polaris.mesh.base.add import add_spherical_base_mesh_step
+from polaris.resolution import (
+    resolution_to_string,
+    resolution_to_string_and_units,
+)
+
+
+class BaseMeshDefinition(NamedTuple):
+    """
+    Immutable metadata for one supported simple base mesh.
+    """
+
+    prefix: str
+    min_res: float
+    max_res: Optional[float] = None
+
+
+def get_base_mesh_step_names():
+    """
+    Get the supported simple base-mesh names in registration order.
+
+    Returns
+    -------
+    mesh_names : tuple of str
+        The supported simple base-mesh names.
+    """
+    return tuple(BASE_MESH_DEFINITIONS)
+
+
+def get_base_mesh_definition(mesh_name):
+    """
+    Get metadata for one supported simple base mesh.
+
+    Parameters
+    ----------
+    mesh_name : str
+        The name of the simple base mesh.
+
+    Returns
+    -------
+    definition : BaseMeshDefinition
+        The mesh metadata needed to rebuild the shared step.
+    """
+    if mesh_name not in BASE_MESH_DEFINITIONS:
+        valid_mesh_names = ', '.join(get_base_mesh_step_names())
+        raise ValueError(
+            f'Unknown simple base mesh {mesh_name}. Valid mesh names are: '
+            f'{valid_mesh_names}'
+        )
+    return BASE_MESH_DEFINITIONS[mesh_name]
 
 
 def get_base_mesh_steps():
@@ -12,35 +61,44 @@ def get_base_mesh_steps():
     base_mesh_steps : list of polaris.mesh.spherical.BaseMeshStep
         All supported base mesh steps in the mesh component
     """
-    uniform_res: Dict[str, List[float]] = {
-        'icos': [480.0, 240.0, 120.0, 60.0, 30.0],
-        'qu': [480.0, 240.0, 210.0, 180.0, 150.0, 120.0, 90.0, 60.0, 30.0],
-    }
-
-    # Add more variable resolution base meshes here
-    variable_res: Dict[str, List[Tuple[float, float]]] = {
-        'so': [(12.0, 30.0)],
-        'rrs': [(6.0, 18.0)],
-    }
+    # Import lazily to avoid a circular import through polaris.tasks.mesh.
+    from polaris.mesh.base.add import add_spherical_base_mesh_step
 
     base_mesh_steps = []
-    for prefix, uniform_res_list in uniform_res.items():
-        for resolution in uniform_res_list:
-            base_mesh_step, _ = add_spherical_base_mesh_step(
-                prefix=prefix, min_res=resolution
-            )
-            base_mesh_steps.append(base_mesh_step)
-
-    for prefix, variable_res_list in variable_res.items():
-        for min_res, max_res in variable_res_list:
-            base_mesh_step, _ = add_spherical_base_mesh_step(
-                prefix=prefix,
-                min_res=min_res,
-                max_res=max_res,
-            )
-            base_mesh_steps.append(base_mesh_step)
+    for mesh_name in get_base_mesh_step_names():
+        definition = get_base_mesh_definition(mesh_name)
+        base_mesh_step, _ = add_spherical_base_mesh_step(
+            prefix=definition.prefix,
+            min_res=definition.min_res,
+            max_res=definition.max_res,
+        )
+        base_mesh_steps.append(base_mesh_step)
 
     return base_mesh_steps
+
+
+def add_spherical_base_mesh_step(
+    prefix,
+    min_res,
+    max_res=None,
+):
+    """
+    Add one supported spherical base mesh step to a component.
+
+    This compatibility wrapper preserves the historic
+    ``polaris.mesh.base.add_spherical_base_mesh_step`` API while importing
+    the lower-level implementation lazily to avoid circular imports.
+    """
+    # Import lazily to avoid a circular import through polaris.tasks.mesh.
+    from polaris.mesh.base.add import (
+        add_spherical_base_mesh_step as add_base_mesh_step,
+    )
+
+    return add_base_mesh_step(
+        prefix=prefix,
+        min_res=min_res,
+        max_res=max_res,
+    )
 
 
 def parse_mesh_filepath(mesh_path):
@@ -48,3 +106,54 @@ def parse_mesh_filepath(mesh_path):
     database = '/'.join(mesh_path.split('/')[1:-1])
     mesh_name = mesh_path.split('/')[-1]
     return component, database, mesh_name
+
+
+def _get_base_mesh_name(prefix, min_res, max_res=None):
+    if max_res is None:
+        res_str = resolution_to_string(min_res)
+    else:
+        min_str, min_units = resolution_to_string_and_units(min_res)
+        max_str, max_units = resolution_to_string_and_units(max_res)
+        if min_units == max_units:
+            min_units = ''
+        res_str = f'{min_str}{min_units}to{max_str}{max_units}'
+    return f'{prefix}{res_str}'
+
+
+def _get_base_mesh_definitions():
+    uniform_res = {
+        'icos': [480.0, 240.0, 120.0, 60.0, 30.0],
+        'qu': [480.0, 240.0, 210.0, 180.0, 150.0, 120.0, 90.0, 60.0, 30.0],
+    }
+
+    variable_res = {
+        'so': [(12.0, 30.0)],
+        'rrs': [(6.0, 18.0)],
+    }
+
+    definitions = {}
+    for prefix, uniform_res_list in uniform_res.items():
+        for resolution in uniform_res_list:
+            mesh_name = _get_base_mesh_name(prefix=prefix, min_res=resolution)
+            definitions[mesh_name] = BaseMeshDefinition(
+                prefix=prefix,
+                min_res=resolution,
+            )
+
+    for prefix, variable_res_list in variable_res.items():
+        for min_res, max_res in variable_res_list:
+            mesh_name = _get_base_mesh_name(
+                prefix=prefix,
+                min_res=min_res,
+                max_res=max_res,
+            )
+            definitions[mesh_name] = BaseMeshDefinition(
+                prefix=prefix,
+                min_res=min_res,
+                max_res=max_res,
+            )
+
+    return definitions
+
+
+BASE_MESH_DEFINITIONS = MappingProxyType(_get_base_mesh_definitions())
--- polaris/mesh/base/add.py
+++ polaris/mesh/base/add.py
@@ -28,10 +28,14 @@ MESH_NAME_PREFIXES = {
 }
 
 
-def add_spherical_base_mesh_step(prefix, min_res, max_res=None):
+def add_spherical_base_mesh_step(
+    prefix,
+    min_res,
+    max_res=None,
+):
     """
     Add a shared step for creating spherical base mesh with the given
-    resolution to the mesh component (if one has not already been added)
+    resolution to a component (if one has not already been added)
 
     Parameters
     ----------
@@ -53,7 +57,39 @@ def add_spherical_base_mesh_step(prefix, min_res, max_res=None):
     res_str : str
         The resolution of the mesh as a string (e.g. '240km' or '12to30km)
     """
-    component = mesh_component
+    (
+        res_str,
+        mesh_name,
+        subdir,
+        config_filename,
+        max_res,
+    ) = _get_base_mesh_info(prefix=prefix, min_res=min_res, max_res=max_res)
+    config = _get_base_mesh_config(
+        component=mesh_component,
+        prefix=prefix,
+        min_res=min_res,
+        max_res=max_res,
+        subdir=subdir,
+        config_filename=config_filename,
+    )
+
+    kwargs = {
+        'step_cls': MESH_CLASSES[prefix],
+        'subdir': subdir,
+        'config': config,
+        'config_filename': config_filename,
+        'name': f'{prefix.lower()}_base_mesh_{res_str}',
+        'mesh_name': mesh_name,
+    }
+    if prefix in ['icos', 'qu']:
+        kwargs['cell_width'] = min_res
+
+    base_mesh = mesh_component.get_or_create_shared_step(**kwargs)
+
+    return base_mesh, res_str
+
+
+def _get_base_mesh_info(prefix, min_res, max_res=None):
     if max_res is None:
         res_str = resolution_to_string(min_res)
         max_res = min_res
@@ -64,32 +100,28 @@ def add_spherical_base_mesh_step(prefix, min_res, max_res=None):
             min_units = ''
         res_str = f'{min_str}{min_units}to{max_str}{max_units}'
 
-    mesh_name = f'{prefix}{res_str}'
     prefix_lower = prefix.lower()
-
-    name = f'{prefix_lower}_base_mesh_{res_str}'
+    mesh_name = f'{prefix}{res_str}'
     subdir = f'spherical/{prefix_lower}/base_mesh/{res_str}'
+    config_filename = f'{prefix_lower}_base_mesh_{res_str}.cfg'
+    return res_str, mesh_name, subdir, config_filename, max_res
 
-    config_filename = f'{name}.cfg'
 
+def _get_base_mesh_config(
+    component,
+    prefix,
+    min_res,
+    max_res,
+    subdir,
+    config_filename,
+):
     filepath = os.path.join(component.name, subdir, config_filename)
+    if filepath in component.configs:
+        return component.configs[filepath]
+
     config = PolarisConfigParser(filepath=filepath)
     config.add_from_package('polaris.mesh.spherical', 'spherical.cfg')
     config.set('spherical_mesh', 'prefix', MESH_NAME_PREFIXES[prefix])
     config.set('spherical_mesh', 'min_cell_width', f'{min_res:g}')
     config.set('spherical_mesh', 'max_cell_width', f'{max_res:g}')
-
-    kwargs = {
-        'step_cls': MESH_CLASSES[prefix],
-        'subdir': subdir,
-        'config': config,
-        'config_filename': config_filename,
-        'name': name,
-        'mesh_name': mesh_name,
-    }
-    if prefix in ['icos', 'qu']:
-        kwargs['cell_width'] = min_res
-
-    base_mesh = component.get_or_create_shared_step(**kwargs)
-
-    return base_mesh, res_str
+    return config
