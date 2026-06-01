ab312ecbaf259b64a762f97833c299dbe4ecf46b
Steven Brus <steven.r.brus@gmail.com>
2026-06-01 13:29:21 -0700

    Fix Omega viz issue

--- polaris/tasks/ocean/baroclinic_channel/viz.py
+++ polaris/tasks/ocean/baroclinic_channel/viz.py
@@ -82,9 +82,10 @@ class Viz(OceanIOStep):
         """
         Run this step of the task
         """
-        ds_mesh = self.open_model_dataset('mesh.nc')
-        ds_init = self.open_model_dataset('init.nc')
-        ds = self.open_model_dataset('output.nc')
+        config = self.config
+        ds_mesh = self.open_model_dataset('mesh.nc', config=config)
+        ds_init = self.open_model_dataset('init.nc', config=config)
+        ds = self.open_model_dataset('output.nc', config=config)
 
         t_index = ds.sizes['Time'] - 1
         cell_mask = ds_init.maxLevelCell >= 1
